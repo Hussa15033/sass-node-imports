18365f879a28a590b7d22665606808146d29a6ce
Auwais Hussain <hussa15033@gmail.com>
2024-03-10 15:47:01 +0000

    Create definition provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,78 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import path from "path";
+import * as vscode from "vscode";
+
+/**
+ * Recursively find the path of the nearest node module directory
+ * @param path
+ */
+const findModulePath = (sassPath: string): Promise<vscode.Uri> => {
+  return new Promise((resolve, reject) => {
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+    if (!workspaceFolder) {
+      reject("Path not found");
+      return;
+    }
+
+    const paths = vscode.workspace.findFiles(
+      path.join("node_modules", sassPath),
+      null,
+      1
+    );
+
+    return paths.then((pathList) => {
+      if (pathList.length === 0) {
+        reject("Path not found");
+      } else {
+        resolve(pathList[0]);
+      }
+    });
+  });
+};
+
+class SassNodeDefinitionProvider implements vscode.DefinitionProvider {
+  public provideDefinition(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken
+  ): Thenable<vscode.Location> {
+    console.log("Something happened...?");
+
+    const importPattern = /["']~(.*?)["']/i;
+    const lineText = document.lineAt(position.line).text;
+
+    const match = importPattern.exec(lineText);
+
+    return new Promise((resolve, reject) => {
+      if (match) {
+        const sassPath = match[1];
+        findModulePath(sassPath)
+          .then((moduleFilePath) => {
+            const definitionLocation = new vscode.Location(
+              vscode.Uri.file(moduleFilePath.fsPath),
+              new vscode.Position(0, 0)
+            );
+            resolve(definitionLocation);
+          })
+          .catch(reject);
+      } else {
+        reject("Path not found");
+      }
+    });
+  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sass-node-imports" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sass-node-imports.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sass Node Imports!');
-	});
-
-	context.subscriptions.push(disposable);
+  console.log("Started extension");
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      { language: "scss", pattern: "**/*.scss" },
+      new SassNodeDefinitionProvider()
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
